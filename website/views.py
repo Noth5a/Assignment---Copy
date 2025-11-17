4c844f2d08ae3f5333eab2dc574ea9879e2ae946
Ashton Wright <ashton.wright@fujitsu.com>
2025-11-17 14:32:40 +0000

    Login required for all routes, update delete logic on ownership, access level is now a choice

--- website/views.py
+++ website/views.py
@@ -17,11 +17,11 @@ def home():
 
 
     print(current_user.role)
-
+    
     if current_user.role == 2:
         requests = Requests.query.all()
     elif current_user.role == 1:
-        requests = Requests.query.filter_by(requester_id=current_user.id).all()
+        requests = Requests.query.filter((Requests.requester_id==current_user.id, Requests.requested_for_email==current_user.email)).all()
     else:
         requests = Requests.query.filter_by(requested_for_email=current_user.email).all()
 
@@ -33,8 +33,9 @@ def home():
         requested_for_email = request.form.get('requested_for_email')
         access_level = request.form.get('access_level')
 
-
-        if len(requested_for_email) < 1 or len(access_level) < 1:
+        if current_user.role == 0 and requested_for_email != current_user.email:
+            flash('Regular Users can only request access for themselves.', category='error')
+        elif len(requested_for_email) < 1 or len(access_level) < 1:
             flash('Request Name and Issuer are required!', category='error')  # Flash an error message if the Certification is empty
         else:
                 
@@ -44,8 +45,8 @@ def home():
                 requested_for_email=requested_for_email,
                 created_at=datetime.now(),
                 last_updated=datetime.now(),
-                access_level=1,
-                requested_for_id=None,
+                access_level=access_level,
+                requested_for_id=User.query.filter_by(email=requested_for_email).first().id if User.query.filter_by(email=requested_for_email).first() else None, 
                 state=0,
                 justification="",
             )
@@ -59,6 +60,7 @@ def home():
 
 # Update request route
 @views.route('/update_request', methods=['PUT','GET'])
+@login_required
 def update_request():
         request_id = request.get_json().get('request_id')
         
@@ -91,6 +93,7 @@ def update_request():
 
         
 @views.route('/Users', methods=['GET'])
+@login_required
 def users():
     if current_user.role != 2:
         flash('You must be an Admin to view users', category='error')
@@ -105,6 +108,7 @@ def users():
 
 # Route to handle note deletion
 @views.route('/delete-Request', methods=['POST'])
+@login_required
 def delete_Request():
     """
     Route for deleting a note via AJAX.
@@ -115,7 +119,7 @@ def delete_Request():
     requestId = data.get('requestId')  
     request_obj = Requests.query.get(requestId)  
     if request_obj:
-        if current_user.role == 1 : 
+        if current_user.role == 1 or current_user.role == 2 : 
             db.session.delete(request_obj)  
             db.session.commit()
             flash('Request deleted.', category = 'success')
